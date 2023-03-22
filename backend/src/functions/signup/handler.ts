4cf66906c38db09b9e386f9b02594916333ea7b5
thejaigoyal <thejaigoyal@gmail.com>
2023-03-22 12:17:07 +0530

    fix: cors in response

--- backend/src/functions/signup/handler.ts
+++ backend/src/functions/signup/handler.ts
@@ -1,87 +1,70 @@
-import { formatJSONResponse } from "@libs/api-gateway";
 import connectDB from "src/config/db";
 import bcrypt from "bcryptjs";
 require("dotenv").config();
 import token from "jsonwebtoken";
 import User from "src/models/user";
+import { formatJSONResponse } from "src/utills/ApiGateway";
+
 export const register: any = async (event) => {
+  console.log("hello");
+  await connectDB();
+  try {
+    const message = JSON.parse(event.body);
+    console.log(message);
+    const saltRounds = 10;
+    const password = message.password;
+    // bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const createuser = new User({
+      fname: message.fname,
+      lname: message.lname,
+      email: message.email,
+      password: hashedPassword,
+    });
     console.log("hello");
-    await connectDB();
-    try {
-
-        const message = JSON.parse(event.body);
-        console.log(message);
-        const saltRounds = 10;
-        const password = message.password;
-        // bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const createuser = new User({
-            fname: message.fname,
-            lname: message.lname,
-            email: message.email,
-            password: hashedPassword
-
-        });
-        console.log("hello")
-        const userdata = await createuser.save();
-        console.log("Successful");
-        return formatJSONResponse({ message: userdata });
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-
-
-
+    const userdata = await createuser.save();
+    console.log("Successful");
+    return formatJSONResponse(200, { data: userdata });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 export const login: any = async (event) => {
+  try {
+    await connectDB();
 
-    try {
-        await connectDB();
-        
-        console.log("Hey");
-        const message = JSON.parse(event.body);
-        console.log(message);
-        let loginuser = await User.findOne({ email: message.email });
-        console.log(loginuser);
-        const result = await bcrypt.compare(message.password, loginuser.password);
-        console.log("hello");
-        if (result == true) {
-            const tokens = token.sign(
-                { email: loginuser.email, id: loginuser._id },
-                
-            );
-            return formatJSONResponse({tokens});
-            
-        }
-        else {
-            const response = {
-                statusCode: 400,
+    console.log("Hey");
+    const message = JSON.parse(event.body);
+    console.log(message);
+    let loginuser = await User.findOne({ email: message.email });
+    console.log(loginuser);
+    const result = await bcrypt.compare(message.password, loginuser.password);
+    console.log("hello");
+    if (result == true) {
+      const tokens = token.sign({ email: loginuser.email, id: loginuser._id });
+      return formatJSONResponse(200, { data: tokens });
+    } else {
+      const response = {
+        statusCode: 400,
 
-                body: JSON.stringify({ message: 'Invalid Detailsssssss' })
-            }; return response;
-
-        }
-
+        body: JSON.stringify({ message: "Invalid Detailsssssss" }),
+      };
+      return response;
     }
-
-    catch (error) {
-        console.log("invalid details");
-    }
+  } catch (error) {
+    console.log("invalid details");
+  }
 };
 export const getuserdata: any = async (event) => {
-    try {
-        console.log("Hey");
-        await connectDB();
-        const e = await User.find();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(e),
-        };
-    }
-    catch (err) {
-        console.log(err.message);
-
-    }
+  try {
+    console.log("Hey");
+    await connectDB();
+    const e = await User.find();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(e),
+    };
+  } catch (err) {
+    console.log(err.message);
+  }
 };
-
